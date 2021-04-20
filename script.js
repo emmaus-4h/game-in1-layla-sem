215de8d6501ab4e8766a56b319d73ef9e6e2d5d4
Sem4HA <>
2021-04-20 11:38:01 +0000

    Pop bewegen

--- script.js
+++ script.js
@@ -115,14 +115,20 @@ var beweegKogel = function () {
  */
 var beweegSpeler = function () {
   if (keyIsDown(KEY_SPACE)) {
-    spelerY = spelerY - 30;
-  
-  spelerY = spelerY - snelheidY;
-  if (spelerY < 550) {
+    spelerY = spelerY - 60;
+  }
+  if (!keyIsDown(KEY_SPACE)) {
+    spelerY = spelerY + 30;
+  }
+
+
+  //spelerY = spelerY - snelheidY;
+  if (spelerY > 550) {
     spelerY = 550;
   }
 
-}};
+
+};
 /**
  * Zoekt uit of de vijand is geraakt
  * @returns {boolean} true als vijand is geraakt
